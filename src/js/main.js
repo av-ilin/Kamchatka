b42ca5ed2d4232d6eb34cf2fd65b29ca337035e9
Андрей Ильин <a.ilin.v@yandex.ru>
2023-04-15 21:05:32 +0300

    upd::header

--- src/js/main.js
+++ src/js/main.js
@@ -86,3 +86,69 @@ new ScrollMagic.Scene({
             )
     )
     .addTo(controller);
+
+// Header
+const headerTiming = 10 * 1e3;
+const headerSwiperProgressElems = {
+    numCurSlide: document.getElementById("header-cur-slide"),
+    numCountSlides: document.getElementById("header-count-slides"),
+};
+
+const headerTimer = {
+    path: document.getElementById("timer-path"),
+    timerId: undefined,
+    frequency: 50,
+    length: headerTiming,
+    start() {
+        let progress = 1;
+        this.timerId = setInterval(() => {
+            progress -= this.frequency / this.length;
+            this.setPath(progress);
+        }, this.frequency);
+    },
+    stop() {
+        this.setPath(0);
+        if (this.timerId) clearInterval(this.timerId);
+        setTimeout(() => {
+            this.setPath(1);
+        }, 400);
+    },
+    setPath(percent) {
+        this.path.style.strokeDasharray = [440 * percent, 440];
+    },
+};
+
+const headerSwiper = new Swiper("#header-bg-swiper", {
+    loop: true,
+    effect: "cards",
+    cardsEffect: {
+        perSlideOffset: 20,
+        perSlideRotate: 4,
+    },
+    autoplay: {
+        delay: headerTiming,
+        disableOnInteraction: false,
+        waitForTransition: true,
+    },
+    speed: 1000,
+    navigation: {
+        nextEl: "#header-bg-swiper-next",
+    },
+    on: {
+        init: function () {
+            headerSwiperProgressElems.numCountSlides.textContent = String(
+                this.slides.length
+            ).padStart(2, "0");
+            headerTimer.start();
+        },
+        slideNextTransitionStart: function () {
+            headerSwiperProgressElems.numCurSlide.textContent = String(
+                this.realIndex + 1
+            ).padStart(2, "0");
+            headerTimer.stop();
+        },
+        slideNextTransitionEnd: function () {
+            headerTimer.start();
+        },
+    },
+});
